f8cfe2b0b2f652c5e6241643efe8e5e09dc61ff0
Kiran Basnet <basnetkiran041@gmail.com>
2025-03-01 18:24:44 +0545

    second commit

---
+++ ArthmeticOperator.java
@@ -0,0 +1,14 @@
+public class ArthmeticOperator {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b =5;
+        System.out.println("The sum is "+ (a+b));
+        System.out.println("The difference is"+(a-b));
+        System.out.println("The product is "+(a*b));
+        System.out.println("The quotient is "+(a/b));
+        System.out.println("The remainder is "+(a%b));
+        System.out.println("The increment of a is "+(a++));
+        System.out.println("The decrement of b is "+(b--));
+    }
+}
